385e8de01bb75ad3e389464f503f5f7910104336
songxuchao <songxuchao2007@126.com>
2017-07-30 09:23:39 +0800

    mysite

---
+++ mysite/view.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*-coding:utf-8-*-
+
+from django.http import HttpResponse
+import datetime
+
+def current_datetime(request):
+	now = datetime.datetime.now()
+	html = "<html><body>It is now %s. </body></html>" % now
+	return HttpResponse(html)
+
